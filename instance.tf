0b4d19aa8f4888796b16c110adbb4dea645c1c4d
sureshny <94158462+sureshny@users.noreply.github.com>
2021-12-02 11:50:18 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key=var.AWS_ACCESS_KEY
+  secret_key=var.AWS_SECRET_KEY 
+}
+resource "aws_instance" "web" {
+  ami           = "ami-04902260ca3d33422"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "unus-HelloWorld"
+  }
+}
